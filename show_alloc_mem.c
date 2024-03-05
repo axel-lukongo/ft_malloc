ae8ba14753bf0f06b3fdcb58bdd4d17de012c98a
axel <lukongo.axel03@gmail.com>
2024-03-05 18:41:56 +0100

    it finish

--- show_alloc_mem.c
+++ show_alloc_mem.c
@@ -20,22 +20,68 @@ void ft_putchar2(char c){
   write(1, &c, 1);
 }
 
-void show_alloc_mem(){
-
-    vm_page_family_t *pg_family = lookup_page_family_by_name("emp_t");
+void show_TYNY(vm_page_family_t *pg_family){
     vm_page_t * vm_page = NULL;
     block_meta_data_t * block_meta = NULL;
-    // char * str = NULL;
+    ft_printf("TYNY : \n");
     if(pg_family->first_page){
       ITERATE_VM_PAGE_BEGIN(pg_family, vm_page)
 
         ITERATE_VM_PAGE_META_BLOCK_BEGIN(vm_page, block_meta)
-        if(block_meta->is_free==FALSE){
-          ft_printf("%p - %p: ", block_meta+1, (block_meta+1)+block_meta->size_of_block);
+        if(block_meta->is_free==FALSE && (block_meta->size_of_block > 0 && block_meta->size_of_block < 100)){
+          ft_printf("%p - %p : ", block_meta+1, (block_meta+1)+block_meta->size_of_block);
           ft_uitoa(block_meta->size_of_block);
         }
         ITERATE_VM_PAGE_META_BLOCK_END(vm_page, block_meta)
 
       ITERATE_VM_PAGE_END(pg_family, vm_page)
     }
+
+}
+
+void show_SMALL(vm_page_family_t *pg_family){
+    vm_page_t * vm_page = NULL;
+    block_meta_data_t * block_meta = NULL;
+    ft_printf("SMALL : \n");
+    if(pg_family->first_page){
+      ITERATE_VM_PAGE_BEGIN(pg_family, vm_page)
+
+        ITERATE_VM_PAGE_META_BLOCK_BEGIN(vm_page, block_meta)
+        if(block_meta->is_free==FALSE && (block_meta->size_of_block > 100 && block_meta->size_of_block < 1000)){
+          ft_printf("%p - %p : ", block_meta+1, (block_meta+1)+block_meta->size_of_block);
+          ft_uitoa(block_meta->size_of_block);
+        }
+        ITERATE_VM_PAGE_META_BLOCK_END(vm_page, block_meta)
+
+      ITERATE_VM_PAGE_END(pg_family, vm_page)
+    }
+
+}
+
+
+void show_LARGE(vm_page_family_t *pg_family){
+    vm_page_t * vm_page = NULL;
+    block_meta_data_t * block_meta = NULL;
+    ft_printf("LARGE : \n");
+    if(pg_family->first_page){
+      ITERATE_VM_PAGE_BEGIN(pg_family, vm_page)
+
+        ITERATE_VM_PAGE_META_BLOCK_BEGIN(vm_page, block_meta)
+        if(block_meta->is_free==FALSE && (block_meta->size_of_block > 1000 && block_meta->size_of_block < 4009)){
+          ft_printf("%p - %p : ", block_meta+1, (block_meta+1)+block_meta->size_of_block);
+          ft_uitoa(block_meta->size_of_block);
+        }
+        ITERATE_VM_PAGE_META_BLOCK_END(vm_page, block_meta)
+
+      ITERATE_VM_PAGE_END(pg_family, vm_page)
+    }
+
+}
+
+
+void show_alloc_mem(){
+    vm_page_family_t *pg_family = lookup_page_family_by_name("emp_t");
+    show_TYNY(pg_family);
+    show_SMALL(pg_family);
+    show_LARGE(pg_family);
 }
--- test_app.c
+++ test_app.c
@@ -8,23 +8,33 @@ int main(){
   void *test3 = malloc(4000);
   void *test4 = malloc(4000);
   void *test5 = malloc(4008);
+  void *test6 = malloc(400);
+  void *test7 = malloc(408);
+  void *test8 = malloc(984);
+  void *test9 = malloc(84);
   char *emp= malloc(5);
   show_alloc_mem();
-  // ft_printf("\n-\n-\n");
+  ft_printf("-\n-\n");
   for(int i = 0; i < 5; i++){
     emp[i] = i + '0';
   }
-  // ft_printf("emp: %s\n", emp);
-  // ft_printf("\n-\n-\n");
+  ft_printf("emp: %s\n", emp);
+  ft_printf("-\n-\n");
   ft_free(emp);
   ft_free(test2);
   ft_free(test1);
   ft_free(test3);
-  // show_alloc_mem();
-  // ft_printf("\n-\n-\n");
+  show_alloc_mem();
+  ft_printf("-\n-\n");
   ft_free(test4);
   ft_free(test5);
-  // show_alloc_mem();
+  show_alloc_mem();
+  ft_printf("-\n-\n");
+  ft_free(test6);
+  ft_free(test7);
+  ft_free(test8);
+  ft_free(test9);
+  show_alloc_mem();
   return 0;
 
 }
